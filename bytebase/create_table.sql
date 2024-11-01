e9ed649bd7bfcede9fc93cdc79a0ac2f70fc68be
huanglang <254453043@qq.com>
2024-11-01 10:12:00 +0800

    测试bytebase

--- bytebase/create_table.sql
+++ bytebase/create_table.sql
@@ -1,29 +1,5 @@
-CREATE TABLE `interface_info` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `name` varchar(256) NOT NULL COMMENT '名称',
-  `description` varchar(256) DEFAULT NULL COMMENT '描述',
-  `url` varchar(512) NOT NULL COMMENT '接口地址',
-  `requestParams` text COMMENT '请求参数',
-  `requestHeader` text COMMENT '请求头',
-  `responseHeader` text COMMENT '响应头',
-  `status` int(11) NOT NULL DEFAULT '0' COMMENT '接口状态（0-关闭，1-开启）',
-  `method` varchar(256) NOT NULL COMMENT '请求类型',
-  `userId` bigint(20) NOT NULL COMMENT '创建人',
-  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  `isDelete` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除(0-未删, 1-已删)',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COMMENT='接口信息';
+CREATE TABLE "public"."t2024" (
+      "id" integer PRIMARY KEY,
+      "name" text
+);
 
-CREATE TABLE `user_interface_info` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `userId` bigint(20) NOT NULL COMMENT '调用用户 id',
-  `interfaceInfoId` bigint(20) NOT NULL COMMENT '接口 id',
-  `totalNum` int(11) NOT NULL DEFAULT '0' COMMENT '总调用次数',
-  `leftNum` int(11) NOT NULL DEFAULT '0' COMMENT '剩余调用次数',
-  `status` int(11) NOT NULL DEFAULT '0' COMMENT '0-正常，1-禁用',
-  `createTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `updateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  `isDelete` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除(0-未删, 1-已删)',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户调用接口关系';
